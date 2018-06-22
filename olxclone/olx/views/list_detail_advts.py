b8650ba028ae966c4d47696f1fdb311aea13dbc3
abishekvanam <abishekvanam@gmail.com>
2018-06-23 01:16:07 +0530

    Corrected code

--- olxclone/olx/views/list_detail_advts.py
+++ olxclone/olx/views/list_detail_advts.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView,DetailView
 class AdvtListView(ListView):
 
     model=Advertisement
-    template_name = 'olx/advt_list.html'
+    template_name = 'olx/advts_list.html'
     context_object_name = 'advt_list'
 
     def get_context_data(self, **kwargs):
