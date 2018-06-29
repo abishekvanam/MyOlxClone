ac5771cb0f217980e780035f79b74c74ef01b526
abishekvanam <abishekvanam@gmail.com>
2018-06-30 00:06:37 +0530

    Added authentication

--- olxclone/olx/rest_views/create_update_delete_api_views.py
+++ olxclone/olx/rest_views/create_update_delete_api_views.py
@@ -1,11 +1,20 @@
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView,RetrieveDestroyAPIView ,DestroyAPIView
 from olx.models import *
 from olx.serializers import *
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+    )
+from olx.permissions import *
 
 
 class AdvtCreateApiView(CreateAPIView):
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
+
+    permission_classes = [IsAuthenticated]#,IsAdminUser]
 
     def perform_create(self, serializer):
         serializer.save(my_user=self.request.user)
@@ -15,6 +24,8 @@ class AdvtUpdateApiView(RetrieveUpdateAPIView):
 
     queryset = Advertisement.objects.all()
     serializer_class = AdvertisementSerializer
+
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly]
 
     def perform_update(self, serializer):
         serializer.save(my_user=self.request.user)
