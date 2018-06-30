c8be652b95686f66bc373c6ddd3fe0ad14a24d6a
abishekvanam <abishekvanam@gmail.com>
2018-07-01 00:13:10 +0530

    Added new fields in serializers

--- olxclone/olx/serializers.py
+++ olxclone/olx/serializers.py
@@ -1,9 +1,39 @@
 from rest_framework import serializers
 from olx.models import *
-
+from rest_framework.serializers import HyperlinkedIdentityField
 
 
 class AdvertisementSerializer(serializers.ModelSerializer):
+    detail_url=HyperlinkedIdentityField(
+        view_name='olx:api_advt_detail_view'
+    )
+
+    update_url=HyperlinkedIdentityField(
+        view_name='olx:api_update_advt_view'
+    )
+
+
+    delete_url=HyperlinkedIdentityField(
+        view_name='olx:api_delete_advt_view'
+    )
+
     class Meta:
         model=Advertisement
-        fields='__all__'
+        fields=(
+            'detail_url',
+            'update_url',
+            'delete_url',
+            'title',
+            'description',
+            'category',
+            'item_type',
+            'price',
+            'image',
+            'posted_on',
+            'interested_count',
+            'my_user')
+
+
+
+
+
